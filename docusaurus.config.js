2b189016607a7f3bb98ca9ec0f828b5aacfc7dd7
ela-iwaszkiewicz <iwaszkiewicz@evolbio.mpg.de>
2026-03-02 20:44:43 +0100

    new_logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
         title: 'DarkTree',
         logo: {
           alt: 'DarkTree Logo',
-          src: 'img/Temp_logo.jpg',
+          src: 'img/DarkTreeLogo.png',
         },
         items: [          
           {
